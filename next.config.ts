29291ce168edc75eb58a464eb22b168e624e019e
Yamma Sarwari <33493912+Yammatast@users.noreply.github.com>
2024-12-09 21:40:02 +0100

    fix build errors

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	/* config options here */
 	images: { domains: ['fakestoreapi.com'] },
-    swcMinify: false,
+    webpack(config, { isServer }) {
+        if (!isServer) {
+          config.optimization.minimize = false;
+        }
+        return config;
+      },
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
 import { Footer, Heading, ToastProvider } from '@/ui/components';
 import { Header } from '@/ui/components/Header';
 import { CartProvider } from '@/context/cartContext';
-
-const geistSans = localFont({
-	src: './fonts/GeistVF.woff',
-	variable: '--font-geist-sans',
-	weight: '100 900',
-});
-const geistMono = localFont({
-	src: './fonts/GeistMonoVF.woff',
-	variable: '--font-geist-mono',
-	weight: '100 900',
-});
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -29,7 +17,6 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
 				<ToastProvider>
 					<CartProvider>
